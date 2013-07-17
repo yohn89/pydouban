06ab164b20517a12b3e462ee864114e5c57c9023
TY@MacBook Pro <tianyu0915@gmail.com>
2013-07-17 14:36:22 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#encoding:utf-8
+
+from setuptools import setup
+
+setup(name='pydouban',  
+      author='TY',  
+      author_email='tianyu0915@gmail.com',  
+      version='0.0.3',  
+      description='A simple python libary for douban.com that contains some basic functions',  
+      keywords ='douban,libary',
+      url='http://github.com/tianyu0915/pydouban',  
+      packages=['pydouban'],  
+      install_requires = ['requests==0.13.1'],
+      license = open('LICENSE').read(),
+
+)  
